69019211b14572e5d779e7c0e6572f50c975d276
REYHAB <rahabwanjiku96@gmail.com>
2023-01-09 12:24:44 +0300

    Updated package.json and uploaded the ebs screenshots

---
+++ zip.js
@@ -0,0 +1,48 @@
+const {createWriteStream } = require('node:fs');
+const archiver = require('archiver');
+const { finished } = require('node:stream/promises');
+(async () => {
+  const buildDir = 'www'
+
+  const output = createWriteStream('./Archive.zip');
+
+  const archive = archiver('zip', {
+    zlib: { level: 9 }, // Sets the compression level.
+  });
+
+  // listen for all archive data to be written
+  // 'close' event is fired only when a file descriptor is involved
+  output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log(
+      'archiver has been finalized and the output file descriptor has closed.',
+    );
+  });
+
+  // This event is fired when the data source is drained no matter what was the data source.
+  // It is not part of this library but rather from the NodeJS Stream API.
+  // @see: https://nodejs.org/api/stream.html#stream_event_end
+  output.on('end', function () {
+    console.log('Data has been drained');
+  });
+
+  // good practice to catch warnings (ie stat failures and other non-blocking errors)
+  archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') console.warn(err.message);
+    else throw err;
+  });
+
+  // good practice to catch this error explicitly
+  archive.on('error', function (err) {
+    throw err;
+  });
+
+  // pipe archive data to the file
+  archive.pipe(output);
+
+  // append files from a sub-directory, putting its contents at the root of archive
+  archive.directory(`./${buildDir}`, false);
+
+  archive.finalize();
+  await finished(output);
+})();
